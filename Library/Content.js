bcfeb0c08c536e6295a507f2f51a48858b7624ec
Adrian Kukowski <adriankukowski@MacBook-Pro-Adrian.local>
2018-12-27 05:07:56 +0100

    Funkcje do wstawiania treści

---
+++ Library/Content.js
@@ -0,0 +1,30 @@
+export default {
+    
+    text(value) {
+
+        if(value !== undefined) {
+
+            return this.each( node => node.textContent = value );
+            
+        } else {
+
+            return this.get(0).textContent;
+
+        }
+
+    },
+
+    html(value) {
+
+        if(value !== undefined) {
+
+            return this.each( node => node.innerHTML = value );
+            
+        } else {
+
+            return this.get(0).innerHTML;
+
+        }
+
+    },
+}
